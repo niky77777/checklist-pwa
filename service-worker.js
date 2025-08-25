76195bae50a50abf85b5c462b847675198d55c5b
niky77777 <grozdevn@yahoo.com>
2025-08-25 15:42:48 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
-const CACHE = "checklist-cache-v3";
+const CACHE = "checklist-cache-act1";
 const ASSETS = [
   "./",
   "./index.html",
-  "./style.css",
-  "./app.js",
-  "./manifest.json",
+  "./style.css?v=act1",
+  "./app.js?v=act1",
+  "./manifest.json?v=act1",
   "./icons/icon-192.png",
   "./icons/icon-512.png"
 ];
@@ -14,7 +14,6 @@ self.addEventListener("install", (e) => {
     caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting())
   );
 });
-
 self.addEventListener("activate", (e) => {
   e.waitUntil(
     caches.keys().then((keys) =>
@@ -22,7 +21,6 @@ self.addEventListener("activate", (e) => {
     ).then(() => self.clients.claim())
   );
 });
-
 self.addEventListener("fetch", (e) => {
   e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request)));
 });
